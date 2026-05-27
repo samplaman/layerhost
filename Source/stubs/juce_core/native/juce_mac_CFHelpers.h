4a0b7947cdaeb4d6ff0e317d237cd5fcb648835d
lali <eoinodowd@outlook.com>
2026-05-27 19:44:54 +0100

    Add macos stub for JUCE 8 clap hosting and revert to ubuntu-latest

---
+++ Source/stubs/juce_core/native/juce_mac_CFHelpers.h
@@ -0,0 +1,8 @@
+// Stub for juce_clap_hosting compatibility with JUCE 8
+// JUCE 8 renamed juce_mac_CFHelpers.h to juce_CFHelpers_mac.h
+
+#pragma once
+
+#if __APPLE__
+#include "juce_CFHelpers_mac.h"
+#endif
